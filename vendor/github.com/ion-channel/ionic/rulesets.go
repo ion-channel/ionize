165aaee3f4ae93f41540218c81d2a7920195a3b2
kellyp <kellyplummer@mac.com>
2017-10-03 15:00:25 -0700

    update for latest ionic/add support for vulnerability scan data

--- vendor/github.com/ion-channel/ionic/rulesets.go
+++ vendor/github.com/ion-channel/ionic/rulesets.go
@@ -15,6 +15,44 @@ const (
 	getRuleSetsEndpoint       = "v1/ruleset/getRulesets"
 )
 
+func (ic *IonClient) GetAppliedRuleSet(id, teamID, analysisID string) (*rulesets.AppliedRulesetSummary, error) {
+	params := &url.Values{}
+	params.Set("project_id", id)
+	params.Set("team_id", teamID)
+	if analysisID != "" {
+		params.Set("analysis_id", analysisID)
+	}
+
+	b, err := ic.get(getAppliedRuleSetEndpoint, params, nil, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get applied ruleset summary: %v", err.Error())
+	}
+
+	var s rulesets.AppliedRulesetSummary
+	err = json.Unmarshal(b, &s)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal applied ruleset summary: %v", err.Error())
+	}
+
+	return &s, nil
+}
+
+func (ic *IonClient) GetRawAppliedRuleSet(id, teamID, analysisID string, page *pagination.Pagination) (json.RawMessage, error) {
+	params := &url.Values{}
+	params.Set("project_id", id)
+	params.Set("team_id", teamID)
+	if analysisID != "" {
+		params.Set("analysis_id", analysisID)
+	}
+
+	b, err := ic.get(getAppliedRuleSetEndpoint, params, nil, page)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get applied rulesets: %v", err.Error())
+	}
+
+	return b, nil
+}
+
 func (ic *IonClient) GetRuleSet(id, teamID string) (*rulesets.RuleSet, error) {
 	params := &url.Values{}
 	params.Set("id", id)
@@ -22,13 +60,13 @@ func (ic *IonClient) GetRuleSet(id, teamID string) (*rulesets.RuleSet, error) {
 
 	b, err := ic.get(getRuleSetEndpoint, params, nil, nil)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get rulesets: %v", err.Error())
+		return nil, fmt.Errorf("failed to get ruleset: %v", err.Error())
 	}
 
 	var rs rulesets.RuleSet
 	err = json.Unmarshal(b, &rs)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get rulesets: %v", err.Error())
+		return nil, fmt.Errorf("failed to unmarshal ruleset: %v", err.Error())
 	}
 
 	return &rs, nil
@@ -46,7 +84,7 @@ func (ic *IonClient) GetRuleSets(teamID string, page *pagination.Pagination) ([]
 	var rs []rulesets.RuleSet
 	err = json.Unmarshal(b, &rs)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get rulesets: %v", err.Error())
+		return nil, fmt.Errorf("failed to unmarshal rulesets: %v", err.Error())
 	}
 
 	return rs, nil
--- vendor/github.com/ion-channel/ionic/rulesets/rulesets.go
+++ vendor/github.com/ion-channel/ionic/rulesets/rulesets.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/ion-channel/ionic/rules"
+	"github.com/ion-channel/ionic/scans"
 )
 
 type RuleSet struct {
@@ -15,4 +16,31 @@ type RuleSet struct {
 	CreatedAt   time.Time    `json:"created_at"`
 	UpdatedAt   time.Time    `json:"updated_at"`
 	Rules       []rules.Rule `json:"rules"`
+}
+
+type AppliedRulesetSummary struct {
+	ID            string              `json:"id"`
+	TeamID        string              `json:"team_id"`
+	ProjectID     string              `json:"project_id"`
+	BuildNumber   string              `json:"build_number"`
+	Name          string              `json:"name"`
+	Text          string              `json:"text"`
+	Type          string              `json:"type"`
+	Source        string              `json:"source"`
+	Branch        string              `json:"branch"`
+	Description   string              `json:"description"`
+	Status        string              `json:"status"`
+	RulesetID     string              `json:"ruleset_id"`
+	CreatedAt     time.Time           `json:"created_at"`
+	UpdatedAt     time.Time           `json:"updated_at"`
+	Duration      float64             `json:"duration"`
+	TriggerHash   string              `json:"trigger_hash"`
+	TriggerText   string              `json:"trigger_text"`
+	TriggerAuthor string              `json:"trigger_author"`
+	ScanSummaries []scans.ScanSummary `json:"scan_summaries"`
+	RulesetName   string              `json:"ruleset_name"`
+	Risk          string              `json:"risk"`
+	Summary       string              `json:"summary"`
+	Trigger       string              `json:"trigger"`
+	Passed        bool                `json:"passed"`
 }
