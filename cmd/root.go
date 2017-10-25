ed8cb520a00af31340b618a30a444124a86239c7
Daniel Hess <dan9186@gmail.com>
2017-10-25 16:29:39 -0700

    dropping extra wrap of function call

--- cmd/root.go
+++ cmd/root.go
@@ -24,15 +24,6 @@ perform supply chain analysis for a project.
 `,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
-	if err := RootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-}
-
 func init() {
 	cobra.OnInitialize(initConfig)
 
--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "github.com/ion-channel/ionize/cmd"
+import (
+	"fmt"
+	"os"
+
+	"github.com/ion-channel/ionize/cmd"
+)
 
 func main() {
-	cmd.Execute()
+	err := cmd.RootCmd.Execute()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
