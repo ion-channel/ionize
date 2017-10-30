d91aed6dafca66f5274dd2b930ddb7a84a7f847e
Daniel Hess <dan9186@gmail.com>
2017-10-29 20:13:31 -0700

    making the writer used testable

--- cmd/configs.go
+++ cmd/configs.go
@@ -15,9 +15,11 @@ var configsCmd = &cobra.Command{
 	Use:   "configs",
 	Short: "Print the configs in use.",
 	Long:  `Print out the configs and their values that have been loaded into ionize.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Config File: %v\n", viper.ConfigFileUsed())
-		fmt.Printf("Secret Key: %v\n", viper.GetString("key"))
-		fmt.Printf("API: %v\n", viper.GetString("api"))
-	},
+	Run:   runConfigsCmd,
+}
+
+func runConfigsCmd(cmd *cobra.Command, args []string) {
+	fmt.Fprintf(output, "Config File: %v\n", viper.ConfigFileUsed())
+	fmt.Fprintf(output, "Secret Key: %v\n", viper.GetString("key"))
+	fmt.Fprintf(output, "API: %v\n", viper.GetString("api"))
 }
