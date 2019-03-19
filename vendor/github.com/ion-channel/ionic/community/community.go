894a9133351a91e125a27f9bd5df4b0e24b34c12
kellyp <kellyplummer@mac.com>
2019-03-19 11:55:12 -0700

    upgrading ionic

--- vendor/github.com/ion-channel/ionic/community/community.go
+++ vendor/github.com/ion-channel/ionic/community/community.go
@@ -3,7 +3,8 @@ package community
 // Repo is a representation of a github repo and corresponding metrics about
 // that repo pulled from github
 type Repo struct {
-	Name       string `json:"name" xml:"name"`
-	URL        string `json:"url" xml:"url"`
-	Committers int    `json:"committers" xml:"committers"`
+	Name       string  `json:"name" xml:"name"`
+	URL        string  `json:"url" xml:"url"`
+	Committers int     `json:"committers" xml:"committers"`
+	Confidence float64 `json:"confidence" xml:"confidence"`
 }
